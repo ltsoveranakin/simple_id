60a855f6a3563a40c75c9a90bf36116905f3b78c
ltsoveranakin <109498476+ltsoveranakin@users.noreply.github.com>
2026-07-24 16:19:49 -0400

    Add property provider

--- src/generator.rs
+++ src/generator.rs
@@ -7,6 +7,44 @@ pub trait IdDataProvider {
     fn get_data(&mut self) -> Data;
 }
 
+pub trait PropertyProvider {
+    fn get_increment(&mut self) -> u8;
+
+    fn get_time(&mut self) -> u16;
+}
+
+#[derive(Default)]
+pub struct StandardPropertyProvider {
+    increment: u8,
+}
+
+impl PropertyProvider for StandardPropertyProvider {
+    fn get_increment(&mut self) -> u8 {
+        if self.increment == 0 {
+            self.increment += 1;
+        }
+
+        let increment = self.increment;
+
+        self.increment += 1;
+        self.increment = self.increment % 0x80;
+
+        increment
+    }
+
+    fn get_time(&mut self) -> u16 {
+        const MAX_TIME: u128 = u16::MAX as u128;
+
+        let time = (SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_nanos()
+            % MAX_TIME) as u16;
+
+        time
+    }
+}
+
 pub struct RandomDataProvider<R> {
     pub rng: R,
 }
@@ -29,44 +67,31 @@ where
     }
 }
 
-pub struct IdGenerator<R> {
-    increment: u8,
-    provider: R,
+pub struct IdGenerator<D, P = StandardPropertyProvider> {
+    data_provider: D,
+    property_provider: P,
 }
 
 pub type RngIdGenerator<R> = IdGenerator<RandomDataProvider<R>>;
 pub type SmallRngIdGenerator = RngIdGenerator<SmallRng>;
 
-impl<DP> IdGenerator<DP>
+impl<D, P> IdGenerator<D, P>
 where
-    DP: IdDataProvider,
+    D: IdDataProvider,
+    P: PropertyProvider,
 {
-    pub fn new(provider: DP) -> Self {
+    pub fn new(data_provider: D, property_provider: P) -> Self {
         Self {
-            increment: 0,
-            provider,
+            data_provider,
+            property_provider,
         }
     }
 
     pub fn generate_new_id(&mut self) -> Id {
-        const MAX_TIME: u128 = u16::MAX as u128;
-
         let header = 0b00000000;
-        let increment = self.increment;
-        let time = (SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos()
-            % MAX_TIME) as u16;
-        let data = self.provider.get_data();
-
-        self.increment += 1;
-        self.increment = self.increment % 0x80;
-
-        // Prevent creating a zero (null) id
-        if self.increment == 0 {
-            self.increment += 1;
-        }
+        let increment = self.property_provider.get_increment();
+        let time = self.property_provider.get_time();
+        let data = self.data_provider.get_data();
 
         Id {
             header,
@@ -77,11 +102,12 @@ where
     }
 }
 
-impl<DP> Default for IdGenerator<DP>
+impl<D, P> Default for IdGenerator<D, P>
 where
-    DP: Default + IdDataProvider,
+    D: Default + IdDataProvider,
+    P: Default + PropertyProvider,
 {
     fn default() -> Self {
-        Self::new(DP::default())
+        Self::new(D::default(), P::default())
     }
 }
